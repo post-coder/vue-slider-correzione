6ff094e6edcb51ba4bbff2e329cbada6fc8bb595
post-coder <gabrielspanu96@gmail.com>
2023-03-15 11:30:44 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,94 @@
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+        
+        slides: [
+            {   
+                image: 'img/01.webp',
+                title: 'Marvel\'s Spiderman Miles Morale',
+                text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+            }, {
+                image: 'img/02.webp',
+                title: 'Ratchet & Clank: Rift Apart',
+                text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+            }, {
+                image: 'img/03.webp',
+                title: 'Fortnite',
+                text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+            }, {
+                image: 'img/04.webp',
+                title: 'Stray',
+                text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+            }, {
+                image: 'img/05.webp',
+                title: "Marvel's Avengers",
+                text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+            }
+        ],
+        
+        activeSlideIndex: 0,
+        
+        timer: null,
+        
+
+    }
+  },
+
+    //   questo metodo viene eseguito al caricamento completo della pagina
+  mounted() {
+    
+    this.startTimer();
+
+  },
+
+
+
+  methods: {
+
+    // mostra la slide in base al parametro passato, utilizzato come indice
+    showSlide(slideIndex) {
+
+        console.log(slideIndex);
+        
+        this.activeSlideIndex = slideIndex;
+
+    },
+
+    startTimer() {
+        this.timer = setInterval(this.nextSlide, 3000);
+    },
+
+    stopTimer() {
+        clearInterval(this.timer);
+    },
+
+    
+    nextSlide() {
+        // aumento il valore di activeSlideIndex
+        if(this.activeSlideIndex >= this.slides.length - 1) {
+            this.activeSlideIndex = 0;
+        } else {
+            this.activeSlideIndex++;
+        }
+        
+    },
+
+    prevSlide() {
+        // diminuisco il valore di activeSlideIndex
+        if(this.activeSlideIndex <= 0) {
+            this.activeSlideIndex = this.slides.length - 1;
+        } else {
+            this.activeSlideIndex--;
+        }
+
+    },
+
+  },
+
+  
+  
+  
+}).mount('#app')
